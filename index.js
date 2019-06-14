7ab12df8e4fec7d6773c4faa7b6d33550ea7e6f4
yusniel Espino <espvtes@gmail.com>
2019-06-13 22:27:39 -0400

    integer, numbers,string

---
+++ index.js
@@ -0,0 +1,27 @@
+// let sample = 7;
+// console.log(typeof sample);
+//espected number
+
+// if you want to output the rounded number or entire
+//or integer number 
+// number you have to do this:
+
+// let sample = 7.77;
+// console.log(parseInt(sample));
+
+//espected 7 instead 8 or 7.77
+
+// for get a float number do this:
+
+// let sample = 7.77;
+// console.log(parseFloat(sample));
+
+//espected 7.77 actually if you write '7.77' the number like
+// this is gonna be 7.77 the same number
+
+
+// if you want to return the 5 decimals for the number 7.77 do this
+
+let sample = 7.77;
+console.log(sample.toFixed(5));
+//espected 7.77000

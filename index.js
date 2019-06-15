eb76e24e42f4158f5d01a2a2bf8fde0f525b455d
yusniel Espino <espvtes@gmail.com>
2019-06-15 12:57:52 -0400

    types coercion

--- index.js
+++ index.js
@@ -120,3 +120,11 @@ var if = 23;*/
 /************************************************** */
 /*Varible mutation and type coercion*/
 
+var firstName = 'John';
+var age = 28;
+console.log(firstName +' '+ age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
